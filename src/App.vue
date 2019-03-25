375fa3eeaaab1a9aa036797b7ed5b1c1a5ed89ee
Vasiliy Khaninev <zenethor@yandex.ru>
2019-03-26 01:17:20 +0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-    <div>
-        <router-view>
-        </router-view>
-    </div>
+  <div>
+    <router-view>
+    </router-view>
+  </div>
 </template>
 
 <script>
-  export default {
-
-  }
+  export default {}
 </script>
 
 <style scoped>
-    div {
-        display: flex;
-        justify-content: center;
-    }
+  div {
+    display: flex;
+    justify-content: center;
+  }
 </style>
